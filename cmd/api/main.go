6b3a67ffaab8294f866d8307a0461cdd143e2fc8
Jeferson de Farias Ferreira <jefersondefariasferreira@gmail.com>
2025-01-27 23:16:55 -0300

    refactor: post campaigns

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,17 +1,14 @@
 package main
 
 import (
-	"emailn/internal/contract"
 	"emailn/internal/domain/campaign"
+	"emailn/internal/endpoints"
 	"emailn/internal/infrastructure/database"
-	internalerrors "emailn/internal/internal-errors"
-	"errors"
 	"fmt"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
-	"github.com/go-chi/render"
 )
 
 var PORT = "3000"
@@ -23,27 +20,13 @@ func main() {
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
 
-	service := campaign.Service{Repository: &database.CampaignRepository{}}
-	r.Post("/campaigns", func(w http.ResponseWriter, r *http.Request) {
-		var request contract.NewCampaign
-		render.DecodeJSON(r.Body, &request)
-
-		id, err := service.Create(request)
-		if err != nil {
-
-			if errors.Is(err, internalerrors.ErrInternal) {
-				render.Status(r, 500)
-			} else {
-				render.Status(r, 400)
-			}
-
-			render.JSON(w, r, map[string]string{"error": err.Error()})
-			return
-		}
-		render.Status(r, 201)
-		render.JSON(w, r, map[string]string{"id": id})
-
-	})
+	service := campaign.Service{
+		Repository: &database.CampaignRepository{},
+	}
+	handler := endpoints.Handler{
+		CampaignService: service,
+	}
+	r.Post("/campaigns", handler.CampaignPost)
 
 	fmt.Println("Listening on port " + PORT)
 	http.ListenAndServe(":"+PORT, r)
